70e782154277c36bae1194440be8fe5f98204def
chitraahuja <CookChoco@46>
2024-11-10 19:51:32 +0530

    test cases added

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,46 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz.math_quiz import select_random_integer_func, select_random_opertaor_func, get_problem_and_output
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_select_random_integer_func(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = select_random_integer_func(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
+    def test_select_random_opertaor_func(self):
+        # Test if operator generated is '+', '-' or '*'
+        operators = ['+', '-', '*']
+        for _ in range(1000):  # Test large number of times
+            random_operator = select_random_opertaor_func()
+            self.assertIn(random_operator, operators)
+
         pass
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_get_problem_and_output(self):
+        # Test if questions and answers generated are correct
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        # test case format (left operand, right operand, operator, expected_problem, expected_answer)
+        test_cases = [
+            (3, 2, '+', '3 + 2', 5),
+            (7, 1, '-', '7 - 1', 6),
+            (9, 5, '*', '9 * 5', 45),
+            (-5, 0, '+', '-5 + 0', -5),
+            (4, 2, '-', '4 - 2', -2),
+            (-3,-4, '*', '-3 * -4', 12),
+            (-4, 2, '*', '-4 * 2', -8)
+        ]
+
+        # check if generated problem & answer matches to the expected problem & answer for each test case
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            generated_problem, generated_answer = get_problem_and_output(num1, num2, operator)
+            self.assertListEqual([generated_problem, generated_answer], [expected_problem, expected_answer])
+
+
 
 if __name__ == "__main__":
     unittest.main()
