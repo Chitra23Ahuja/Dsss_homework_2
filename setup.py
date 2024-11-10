a923eb0de7a225e42dabcd3f2c802f1d4dd79b4f
chitraahuja <CookChoco@46>
2024-11-10 19:24:29 +0530

    initial setup and math quiz folder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from distutils.core import setup
+setup(
+    name="math_quiz",
+    version="1.0",
+    author="Chitra Ahuja",
+    author_email="chitraahuja23@gmail.com",
+    packages=find_packages(),
+    description='setup.py for Math Quiz'
+)
